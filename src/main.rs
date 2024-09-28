5a60631e92cc37402fc99d3f5ec5004f35abfe0c
ImonaMobilTeam <Rahim47@users.noreply.github.com>
2024-09-28 23:19:51 +0300

    Add to remote Github

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+// This is a simple API for dog walking business
+// run command: cargo watch -c src -x run
+
+mod models;
+mod routes;
+mod services;
+
+use actix_web::{get, web::Data, App, HttpResponse, HttpServer, Responder};
+use routes::{
+    booking_route::{cancel_booking, create_booking, get_bookings},
+    dog_route::create_dog,
+    owner_route::create_owner,
+};
+use services::db::Database;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello Human!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db = Database::init().await;
+    let db_data = Data::new(db);
+    HttpServer::new(move || {
+        App::new()
+            .app_data(db_data.clone())
+            .service(hello)
+            .service(create_owner)
+            .service(create_dog)
+            .service(create_booking)
+            .service(get_bookings)
+            .service(cancel_booking)
+    })
+    .bind(("127.0.0.1", 5001))?
+    .run()
+    .await
+}
